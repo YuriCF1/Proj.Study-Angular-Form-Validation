0af1dd8d6a388816bebf5e71d97439cced652ab4
Yuri Cruz <yuricruzf@gmail.com>
2023-12-15 12:47:19 -0300

    Implementing directive

--- forms-angular-main/src/app/app.module.ts
+++ forms-angular-main/src/app/app.module.ts
@@ -9,9 +9,10 @@ import { CadastroComponent } from './cadastro/cadastro.component';
 import { SucessoCadastroComponent } from './sucesso-cadastro/sucesso-cadastro.component';
 import { FormsModule } from '@angular/forms';
 import { ErrorMessageComponent } from './components/error-message/error-message.component';
+import { UnderAngeFilterDirective } from './directives/under-ange-filter.directive';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, CadastroComponent, SucessoCadastroComponent, ErrorMessageComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, CadastroComponent, SucessoCadastroComponent, ErrorMessageComponent, UnderAngeFilterDirective],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
