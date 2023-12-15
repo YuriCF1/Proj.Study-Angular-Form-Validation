43cb9452670a115af42ff6790a53f2c3a0030bd6
Yuri Cruz <yuricruzf@gmail.com>
2023-12-15 14:38:11 -0300

    Creating functionto get adress bry zip code

--- forms-angular-main/src/app/cadastro/cadastro.component.ts
+++ forms-angular-main/src/app/cadastro/cadastro.component.ts
@@ -1,5 +1,7 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ConsultaCepService } from '../services/consulta-cep.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -8,12 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private cepService: ConsultaCepService
+  ) { }
 
   ngOnInit(): void {
   }
 
-  cadastrar(){
-      console.log('Formulário enviado');
+  register(form: NgForm) {
+    // if (form.valid) {
+    //   this.router.navigate(['./sucesso'])
+    // } else {
+    //   alert("Form invalido")
+    // }
+    // console.log(form);
+    console.log(form.controls);
+  }
+
+  consultaCep(ev: FocusEvent) {
+    const inputElement = ev.target as HTMLInputElement;
+    const cepValor = inputElement.value;
+
+    if (cepValor) {
+      this.cepService.getConsultaCep(cepValor).subscribe(resultado => {
+        console.log(resultado);
+      });
+    }
   }
 }
